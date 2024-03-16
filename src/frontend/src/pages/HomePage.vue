a4f1928addceba226431e4dba1c1997975fee746
stvoidit <stvoid.it@yandex.ru>
2024-03-16 21:34:31 +0300

    таблица в компонент

--- src/frontend/src/pages/HomePage.vue
+++ src/frontend/src/pages/HomePage.vue
@@ -6,22 +6,9 @@
                 @submit="fetchReportOption" />
         </el-col>
     </el-row>
-
     <el-row style="margin-top: 2em;">
         <el-col>
-            <el-card>
-                <el-table
-                    :data="reportsLog"
-                    border
-                    flexible
-                    size="small"
-                    style="width: 100%">
-                    <el-table-column
-                        v-for="column in columns"
-                        :key="column.id"
-                        v-bind="column" />
-                </el-table>
-            </el-card>
+            <TableReports :data="reportsLog" />
         </el-col>
     </el-row>
 </template>
@@ -29,7 +16,7 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import ReportForm from "@/pages/components/ReportForm.vue";
-import dayjs from "dayjs";
+import TableReports from "@/pages/components/TableReports.vue";
 import api from "@/api";
 const reportsLog = ref([]);
 const reportOptions = ref([]);
@@ -38,43 +25,4 @@ const fetchReportOption = async () => await Promise.all([
     api.getReportsLog().then(data => reportsLog.value = data)
 ]);
 onMounted(fetchReportOption);
-const columns = [
-    {
-        label: "Дата отчета",
-        prop: "report_date",
-        formatter: (row) => dayjs(row.report_date).format("DD.MM.YYYY")
-    },
-    {
-        label: "Дата отправки отчета",
-        prop: "created",
-        formatter: (row) => new Date(row.created).toLocaleString()
-    },
-    {
-        label: "Пользователь",
-        prop: "login"
-    },
-    {
-        label: "Продавец",
-        prop: "saler"
-    },
-    {
-        label: "Торговая точка",
-        prop: "retail_outlet"
-    },
-    {
-        label: "Вал",
-        prop: "val",
-        formatter: (row) => row.val.toLocaleString()
-    },
-    {
-        label: "Бл",
-        prop: "bl",
-        formatter: (row) => row.bl.toLocaleString()
-    },
-    {
-        label: "Нал",
-        prop: "nal",
-        formatter: (row) => row.nal.toLocaleString()
-    }
-];
 </script>
---
+++ src/frontend/src/pages/components/TableReports.vue
@@ -0,0 +1,60 @@
+<template>
+    <el-table
+        :data="data"
+        border
+        flexible
+        size="small"
+        style="width: 100%">
+        <el-table-column
+            v-for="column in columns"
+            :key="column.id"
+            v-bind="column" />
+    </el-table>
+</template>
+
+<script setup>
+import dayjs from "dayjs";
+defineProps({
+    data: { type: Array, required: true}
+});
+const columns = [
+    {
+        label: "Дата отчета",
+        prop: "report_date",
+        formatter: (row) => dayjs(row.report_date).format("DD.MM.YYYY")
+    },
+    {
+        label: "Дата отправки отчета",
+        prop: "created",
+        formatter: (row) => new Date(row.created).toLocaleString()
+    },
+    {
+        label: "Пользователь",
+        prop: "login"
+    },
+    {
+        label: "Продавец",
+        prop: "saler"
+    },
+    {
+        label: "Торговая точка",
+        prop: "retail_outlet"
+    },
+    {
+        label: "Вал",
+        prop: "val",
+        formatter: (row) => row.val.toLocaleString()
+    },
+    {
+        label: "Бл",
+        prop: "bl",
+        formatter: (row) => row.bl.toLocaleString()
+    },
+    {
+        label: "Нал",
+        prop: "nal",
+        formatter: (row) => row.nal.toLocaleString()
+    }
+];
+
+</script>
